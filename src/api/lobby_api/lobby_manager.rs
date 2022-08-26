0d77c191c46a261abe5f885f661e42b19a7440a1
Alan Zhu <alan.zhu39@gmail.com>
2022-08-26 00:36:33 -0700

    adding some lobby manager skeleton

---
+++ src/api/lobby_api/lobby_manager.rs
@@ -0,0 +1,20 @@
+use crate::lobby;
+
+pub struct LobbyManager {
+  // TODO:
+  // Lobby maps
+}
+
+impl LobbyManager {
+  pub fn new() {
+    // TODO
+  }
+
+  pub fn create_lobby() {
+    // TODO
+  }
+
+  pub fn join_lobby() {
+    // TODO
+  }
+}
--- src/api/lobby_api/mod.rs
+++ src/api/lobby_api/mod.rs
@@ -1,3 +1,12 @@
 mod lobby_manager
 
-pub fn 
+use actix_web::web;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+  cfg.service(
+      web::resource("/")
+        .route(web::get().to(HttpResponse::Ok))
+        .route(web::head().to(HttpResponse::MethodNotAllowed)),
+  )
+  .service(web::scope("/lobby").configure(lobby_manager::config));
+}
